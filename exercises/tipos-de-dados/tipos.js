a6003550d9aae2505f7f58b452b3081fc487ec2e
diandrade <contato.andradediego@gmail.com>
2025-08-06 15:55:03 -0300

    Finalização dos primeiros exercícios de JS, e criação de pastas para os próximos.

---
+++ exercises/tipos-de-dados/tipos.js
@@ -0,0 +1,59 @@
+/*Crie uma variável chamada nome e armazene seu nome nela.
+Em seguida, crie uma variável idade e atribua um número representando sua
+idade. Exiba no console a seguinte frase usando template literals:
+
+nome = "Diego";
+idade = 23;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);*/
+
+/*Crie duas variáveis num1 e num2, atribua valores numéricos a elas e realize as
+operações soma, subtração, multiplicação e divisão. Exiba os resultados no
+console.
+
+num1 = 1;
+num2 = 2;
+console.log(`Soma: ${num1 + num2}`);
+console.log(`Subtração: ${num1 - num2}`);
+console.log(`Multiplicação: ${num1 * num2}`);
+console.log(`Divisão: ${num1 / num2}`);
+*/
+
+/*Crie duas variáveis a e b com valores numéricos. Utilize operadores de
+comparação (==, ===, >, <, >=, <=) e exiba os resultados no console.
+
+num1 = 1;
+num2 = 2;
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+*/
+
+/*Crie um array chamado frutas contendo pelo menos 4 frutas. Adicione mais uma
+fruta ao final, remova a primeira fruta e exiba o array atualizado no console. 
+
+var frutas = ["Morango", "Laranja", "Uva", "Maracujá"];
+frutas.push("Pêssego");
+frutas.splice(0, 1);
+console.log(frutas);
+*/
+
+/*Crie um objeto chamado carro com as propriedades marca, modelo e ano. Exiba
+no console os valores dessas propriedades e adicione dinamicamente uma nova
+propriedade chamada cor.
+
+var carro = {marca: 'Ford', modelo: 'Uno', ano: '2022'};
+
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+
+carro.cor = 'Verde';
+console.log(carro.cor);
+*/
+
+
+
+
